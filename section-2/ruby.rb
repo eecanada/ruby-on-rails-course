e4adf08f45b7edb854e591aa84d0c8ba59a47f73
Eder Canada <canada.eder@gmail.com>
2020-01-18 16:41:12 -0800

    introduction

--- section-2/ruby.rb
+++ section-2/ruby.rb
@@ -1,3 +1,23 @@
-address = [1,2,3,3,4]
+####################Introduction######################
 
-p address 
+print "Hello World" #does not give me a new line 
+puts "Hello World" #acts as a return 
+p "Hello World"
+
+greeting = "Hello Eder"
+p greeting
+
+# this is a method say_hello
+def say_hello
+  p "Hello Ed" 
+end
+say_hello #calling the method  
+
+# this is how to pass an arguement to the method
+def say_hello(things_to_say)
+  p things_to_say 
+end
+say_hello("I am a Ruby code master") #calling the method  
+
+
+####################WORKING WITH STRING######################
