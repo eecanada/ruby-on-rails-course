1fd60fafd14c8a816058a6b00a21134f3e0bba8f
Eder Canada <canada.eder@gmail.com>
2020-01-21 10:45:22 -0800

    working with numbers, float, integer methods, and modulus operator

--- section-2/ruby.rb
+++ section-2/ruby.rb
@@ -61,12 +61,46 @@
 # p input.to_i * 2 # I use .to_i because input is a string right now and I need to convert it to a number 
 
 
-p "What is your first name?"
-first_name = gets.chomp 
-p "What is your last name?"
-last_name = gets.chomp
-p "Your full name is #{first_name} #{last_name}"
-full_name = "#{first_name} #{last_name}"
-p "Your full name is reversed is #{full_name.reverse}"
-p "Your name has #{full_name.delete(" ").length} character"
+# p "What is your first name?"
+# first_name = gets.chomp 
+# p "What is your last name?"
+# last_name = gets.chomp
+# p "Your full name is #{first_name} #{last_name}"
+# full_name = "#{first_name} #{last_name}"
+# p "Your full name reversed is #{full_name.reverse}"
+# p "Your name has #{full_name.delete(" ").length} character"
+
+
+###############################################WORKING WITH NUMBERS ###################################################
+
+p 1 + 2
+p 2 * 4 
+p 10 / 4 #--> 2 drops the decimal so equals 
+p 10.0 / 4 #--> 2.5 have to turn one of the numbers into a float for division to work or 10/4.0
+p 10 / 4.to_f #--> 2.5
+x = 5
+y = 10
+p x + y #--> 15
+
+p "I am a line"
+p "-" * 20 # I can mupilty the string and a number, but I can do 20 * "-" since an number cannot be multiplied by "-"
+p "I am a different line after the divider"
+
+20.times {p "hi"} #--> logs out hi 20 times. A method that allows me to do something x ammount of times
+20.times {p rand(10)} #--> logs out random number between 0 and 9, 20 times
+
+p x.to_f #-->5.0
+p "hello".to_f #--> 0, cannot convert string to a float, so it returns a 0
+
+p "Simple Calculator"
+25.times { print "-" } #print does not create a new line, as opposed to p which does 
+puts
+p "Enter the first number"
+num_1 = gets.chomp
+p "Enter the second number"
+num_2 = gets.chomp
+p "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
+p "The remainder of the first number divided by the second number is #{num_1.to_i % num_2.to_i}"
+
+p 19 % 4 # --> 3, leave the reminder of the division 
 
